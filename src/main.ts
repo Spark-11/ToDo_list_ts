c3330ff30f601bb70c2e7c2c0241665188dd0139
Spark-11 <akshaysaini2003@gmail.com>
2024-08-25 21:30:45 +0530

    Add files

---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import './style.css'
+
+interface Todo{
+  title:string;
+  isCompleted:boolean;
+  readonly id : string;
+}
+const todos : Todo[] = []
+const todosContainer  = document.querySelector(".todosContainer") as HTMLDivElement;
+const todoInput = document.getElementsByTagName("input")[0] as HTMLInputElement
+const myForm = document.getElementById("myForm") as HTMLFormElement
+
+myForm.onsubmit = (e:SubmitEvent) => {
+  e.preventDefault();
+  const todo : Todo = {
+    title : todoInput.value,
+    isCompleted:false,
+    id: String(Math.random()*100)
+  }
+  todos.push(todo)
+  todoInput.value = ""
+  renderTodo(todos)
+}
+
+const renderTodo = (todos:Todo[]) =>{
+  todosContainer.innerText = ""
+  todos.forEach((item) => {
+    generateTodoItem(item.title,item.isCompleted,item.id)
+  })
+}
+
+const generateTodoItem = (title:string,isCompleted:boolean,id:string) =>{
+  const todo: HTMLDivElement = document.createElement("div")
+  todo.className = "todo"
+  //Creating checkBox
+  const checkBox : HTMLInputElement  = document.createElement("input")
+  checkBox.style.cursor = "pointer"
+  checkBox.setAttribute("type","checkbox")
+  checkBox.className = "isCompleted"
+  checkBox.checked = isCompleted
+  checkBox.onchange = () =>{
+    todos.find((item) =>{
+      if(item.id ===id) item.isCompleted = checkBox.checked
+      paragraph.className = checkBox.checked ? "textCut":""
+    })
+  }
+  
+  //Creating paragraph for title
+  const paragraph : HTMLParagraphElement = document.createElement("p") 
+  paragraph.innerText = title
+  paragraph.className = checkBox.checked ? "textCut":""
+
+  //Creating Delete Button
+  const btn : HTMLButtonElement = document.createElement("button")
+  btn.innerText = "X"
+  btn.className = "deleteBtn"
+  btn.onclick = ()  =>{
+    deleteBtn(id)
+  }
+
+  //Append all the items
+  todo.append(checkBox,paragraph,btn)
+  todosContainer.append(todo)
+
+}
+ const deleteBtn = (id:string) =>{
+  const idx = todos.findIndex(item=>item.id===id)
+  todos.splice(idx,1)
+  renderTodo(todos)
+ }
